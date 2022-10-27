92c1c7cdf3e353a0e5120e831fbd96357e25f7b0
DosTorrez <303dos18@gmail.com>
2022-10-27 15:39:58 -0600

    schema folder added

---
+++ Develop/server/schemas/typeDefs.js
@@ -0,0 +1,49 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+type Query {
+    me: User 
+}
+
+type User {
+    _id: ID!
+    username: String!
+    email: String!
+    password: String!
+    bookCount: Int 
+    savedBooks: [Book]
+}
+
+type Auth {
+    token: ID!
+    user: User
+}
+
+type Book {
+    _id: ID
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+}
+
+input saveBookInput {
+    bookId: String
+    authors: [String]
+    title: String
+    description: String
+    image: String
+    link: String
+}
+
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    savedBook(body: saveBookInput): User
+    removeBook(bookId: String!): User
+}
+`;
+
+module.exports = typeDefs; 
